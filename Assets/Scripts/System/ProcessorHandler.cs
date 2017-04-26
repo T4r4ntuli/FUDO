11806d1babb34725808fb2c892cf3b3ec7e65452
Janne Laine <jannelain@gmail.com>
2017-04-26 21:01:09 +0300

    Added processors

---
+++ Assets/Scripts/System/ProcessorHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fudo
+{
+    public class ProcessorHandler : MonoBehaviour
+    {
+        Fudo.PRC.MovementProcessor movement;
+        ComponentManager componentManager;
+        GenericDictionary<Vector3> positions;
+        GenericDictionary<Components.Movement> movementComponents;
+
+        // Use this for initialization
+        void Start() {
+            componentManager = ComponentManager.Instance;
+            //positions = componentManager.positions;
+            //movementComponents = componentManager.movementComponents;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            movement.Process(positions, movementComponents);
+        }
+
+        void LateUpdate() {
+
+        }
+    }
+
+}
