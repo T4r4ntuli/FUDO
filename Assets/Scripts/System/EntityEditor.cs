d80e4f5a5521f6d519b305d3cac536bcf7243cfd
Hakhyun Kim <hakhyun.kim@criticalforce.fi>
2017-09-08 12:59:21 +0300

    Added. GUI handler for the GameObject with Entity script is added to it.

--- Assets/Scripts/System/EntityEditor.cs
+++ Assets/Scripts/System/EntityEditor.cs
@@ -1,17 +1,87 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using Fudo.Enums;
+using UnityEngine;
 using UnityEditor;
 
 namespace Fudo.Drawer {
     [CustomEditor(typeof(Entity))]
     public class EntityEditor : Editor {
+        public Enums.ComponentType componentTypeToSet;
         public override void OnInspectorGUI() {
+            if (target == null)
+                return;
             Entity e = (Entity)target;
-            EditorGUILayout.IntField("EntityID", e.id);
 
-            if (ComponentManager.Instance == null) {
+            if (EntityManager.Instance == null || ComponentManager.Instance == null) {
                 return;
             }
+
+            PlayerManager playerManager = PlayerManager.Instance;
+            EntityManager entityManager = EntityManager.Instance;
             ComponentManager componentManager = ComponentManager.Instance;
+            
+            if (e.id == 0 || entityManager.entities.ContainsKey(e.id) == false) {
+                if (GUILayout.Button("Create Entity"))
+                {
+                    e.id = entityManager.GenerateEntityID();
+                    e.components = new List<ComponentType>();
+                    entityManager.entities.Add(e.id, e);
+                    componentManager.entityGameObjects.Add(e.id, e.gameObject);
+                    componentManager.entityTransforms.Add(e.id, e.transform);
+                    componentManager.rigidbodies.Add(e.id, e.GetComponent<Rigidbody>());
+                }
+                return;
+            }
+            else {
+                if (GUILayout.Button("Delete Entity")) {
+                    entityManager.DeleteEntity(e.id);
+                    return;
+                }
+            }
+
+            EditorGUILayout.IntField("EntityID", e.id);
+            EditorGUILayout.BeginHorizontal();
+            componentTypeToSet = (Fudo.Enums.ComponentType)EditorGUILayout.EnumPopup("Component to create:", componentTypeToSet);
+            if (GUILayout.Button("Create"))
+            {
+                if (e.components.Contains(componentTypeToSet) == false)
+                {
+                    switch (componentTypeToSet) {
+                        case Enums.ComponentType.Position:
+                            componentManager.AddComponent(Enums.ComponentType.Position, Vector3.zero, e.id);
+                            break;
+                        case Enums.ComponentType.Scale:
+                            componentManager.AddComponent(Enums.ComponentType.Scale, Vector3.one, e.id);
+                            break;
+                        case Enums.ComponentType.Rotation:
+                            componentManager.AddComponent(Enums.ComponentType.Rotation, Quaternion.identity, e.id);
+                            break;
+                        case Enums.ComponentType.Direction:
+                            componentManager.AddComponent(Enums.ComponentType.Direction, Vector3.forward, e.id);
+                            break;
+                        case Enums.ComponentType.MaxSpeed:
+                            componentManager.AddComponent(Enums.ComponentType.MaxSpeed, 5.0f, e.id);
+                            break;
+                        case Enums.ComponentType.Movement:
+                            componentManager.AddComponent(Enums.ComponentType.Movement, new Components.Movement(),
+                                e.id);
+                            break;
+                        case Enums.ComponentType.InputToMovement:
+                            componentManager.AddComponent(Enums.ComponentType.InputToMovement,
+                                new Components.MovementInput(), e.id);
+                            break;
+                    }
+                }
+            }
+
+            if (GUILayout.Button("Remove"))
+            {
+                if (e.components.Contains(componentTypeToSet)) {
+                    componentManager.RemoveComponent(componentTypeToSet, e.id);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
 
             foreach (Enums.ComponentType componentType in e.components) {
                 EditorGUILayout.Space();
